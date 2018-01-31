0edf4eb25c73c2fc636a5ddb4848b1880f138f3d
devilk87 <kirill.nazarenko@gmail.com>
2018-01-31 19:47:24 +0200

    refactoring

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# uber-test
-Test for Uber API
+# AfterUber
+Search available car in AfterUber with 20% discount 
+
+### Installation
+* Add `export NODE_ENV=development` into .bash_profile, and open new terminal session.
+* Add the following into .bash_profile, and open new terminal session:
+ `export googleApiKey=YOUR_GOOGLE_API_KEY
+  export uberServerToken=YOUR_UBER_SERVER_TOKEN`
+
+### Running
+```
+npm install -g nodemon
+npm run app
+```
+### Testing
+```
+npm test
+```
--- app.js
+++ app.js
@@ -4,22 +4,28 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const process = require('process');
+const config = require('config');
+
 
 const frontController = require('./lib/controllers/front-controller');
 const uberController = require('./lib/controllers/uber-controller');
 const googleController = require('./lib/controllers/google-controller');
 
+
 const app = express();
-const port = 3599;
+const port = config.port;
 
 app.listen(port);
 
+console.log(`App started at port:${port} ENV:${process.env.NODE_ENV}`);
 
 app.use(bodyParser.json({ type: 'application/json'}));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', frontController.index);
-app.get('/afteruber/prices', uberController.uberGet);
+app.get('/afteruber/prices', uberController.getPrices);
 app.get('/:language/address/:address', googleController.geocoding);
 
+exports.app = app;
 
