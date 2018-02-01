499d907818987e02a159f0afcf32ecd6e0007973
devilk87 <kirill.nazarenko@gmail.com>
2018-02-01 11:57:55 +0200

    Front refactoring. Controller loader

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ const process = require('process');
 const config = require('config');
 const expressValidator = require('express-validator');
 
-const frontController = require('./lib/controllers/front-controller');
-const uberController = require('./lib/controllers/uber-controller');
-const googleController = require('./lib/controllers/google-controller');
+const controllersLoader = require('./lib/helpers/controllers-loader');
 
 const app = express();
 const port = config.port;
@@ -23,9 +21,8 @@ app.use(bodyParser.json({ type: 'application/json'}));
 app.use(express.static(__dirname + '/public'));
 app.use(expressValidator());
 
-frontController.controller(app);
-uberController.controller(app);
-googleController.controller(app);
+
+controllersLoader.gatherControllers(app, __dirname);
 
 exports.app = app;
 
